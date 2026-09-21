2d3f56197c8cc37b70adc754c87ccb86b2298a7b
Preet <preet382415@gmail.com>
2026-09-21 16:08:11 +0530

    Add Restore IP Addresses Solution

---
+++ 93_Restore_IP_Addresses.py
@@ -0,0 +1,31 @@
+class Solution:
+    def restoreIpAddresses(self, s):
+        result = []
+
+        def backtrack(index, parts):
+            if len(parts) == 4:
+                if index == len(s):
+                    result.append(".".join(parts))
+                return
+
+            for length in range(1, 4):
+                if index + length > len(s):
+                    break
+
+                part = s[index:index + length]
+
+                if len(part) > 1 and part[0] == '0':
+                    continue
+
+                if int(part) > 255:
+                    continue
+
+                parts.append(part)
+
+                backtrack(index + length, parts)
+
+                parts.pop()
+
+        backtrack(0, [])
+
+        return result
